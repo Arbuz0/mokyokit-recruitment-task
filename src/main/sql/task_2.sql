42c4598570195ba28dea9be6900ddfb13472d670
Łukasz Janiak <lucasekv2@gmail.com>
2023-07-24 20:52:44 +0200

    Format task 2

--- src/main/sql/task_2.sql
+++ src/main/sql/task_2.sql
@@ -1,25 +1,18 @@
 SELECT
     klient_id,
     status
-FROM
-    processed_statuses
+FROM processed_statuses
 WHERE
     klient_id IN (
-        SELECT
-            klient_id
-        FROM
-            statuses
-        GROUP BY
-            klient_id
-        HAVING
-            Count(*) > 3
+        SELECT klient_id
+        FROM statuses
+        GROUP BY klient_id
+        HAVING Count(*) > 3
     )
     AND (klient_id, kontakt_ts) IN (
         SELECT
             klient_id,
             MAX(kontakt_ts)
-        FROM
-            statuses
-        GROUP BY
-            klient_id
-    )
+        FROM statuses
+        GROUP BY klient_id
+    );
